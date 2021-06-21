d1989c50c317524f33f4af3ad95ef657716f3339
Gustavo Castro <gustavocastrocs@gmail.com>
2021-06-21 10:34:21 -0300

    Variaveis

---
+++ guia-javascript/tipos-dados/script.js
@@ -0,0 +1,41 @@
+//String
+
+const nome = 'Gustavo' ;
+console.log(nome)
+
+//Number
+
+let idade = 25
+console.log(idade)
+
+//Boolean
+
+const possuiFaculdade = true;
+
+//Undefined vs Null
+//Null: Objeto que nao possui nada dentro, mas existe, diferente de Undefined
+//Undefined: Algo que nao existe
+
+//Object: Dado estrutural
+//Propriedades -> Atributos
+//Funcionalidades -> Metodos
+
+console.log({
+  name: 'Gustavo',
+  idade: 25,
+  andar: function(){
+    console.log(andar)
+  }
+})
+
+
+//Array
+const compras = ['Ovos', 'Leite', 'Pao']
+
+//Data Types:
+//Primitive -> String, Number, boolean, undefined, symbol, BigInt
+
+//Structural -> Object (Array, map, set, list)
+
+//Structural Root Primite -> Null
+
